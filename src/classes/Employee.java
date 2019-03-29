ad96d4201fd4b7fc9fbf1a39ad007699db512e67
Ivan <ivanmorte3@iesmontsia.org>
2019-03-29 16:38:26 +0100

    create connect employee

--- src/classes/Employee.java
+++ src/classes/Employee.java
@@ -4,11 +4,32 @@
  * and open the template in the editor.
  */
 package classes;
-
+import java.util.Date;
+import java.sql.Timestamp;
 /**
  *
  * @author root
  */
 public class Employee {
+    private int id;
+    private String nom;
+    private String cognom1;
+    private String cognom2;
+    private String email;
+    private Timestamp email_verified_at;
+    private String password;
+    private Date data_naixement;
+    private String adreca;
+    private String ciutat;
+    private String provincia;
+    private String codi_postal;
+    private String tipus_document;
+    private String numero_document;
+    private String sexe;
+    private String telefon;
+    private int id_rol;
+    private int id_dades_empleat;
+    private 
+    
     
 }
--- src/connect/DBEmployee.java
+++ src/connect/DBEmployee.java
@@ -5,10 +5,34 @@
  */
 package connect;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author root
  */
 public class DBEmployee {
+    Connection connect = null;
+    String url = "jdbc:mysql://localhost:3306/univeylandia_laravel";
+    String user = "root";
+    String password = "alumne";
+    
+    public DBEmployee(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection(url, user, password);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public Connection getConnect(){
+        return connect;
+    }
+    
+    public void disconnect(){
+        connect = null;
+    }
     
 }
